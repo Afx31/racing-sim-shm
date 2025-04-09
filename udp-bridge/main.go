c1b2edc0215cedf94432fc8bfff4a81ca76afffc
Afx31 <post4wjm@gmail.com>
2025-04-09 21:06:17 +1000

    Refined reading data

--- udp-bridge/main.go
+++ udp-bridge/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	"encoding/binary"
 	"fmt"
-	"net"
+
+	// "net"
 
 	// "os"
 	"sync"
@@ -30,34 +31,48 @@ var (
 func ReadSharedMemory(physics *accshmdata.ACCPhysics) {
 	fmt.Println("--- Reading Memory ---")
 	
-	// Setup ticker for looping beloiw
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
 	for {
 		<-ticker.C
 		physicsSize := (int32)(unsafe.Sizeof(*physics))
-		rbuf := make([]byte, physicsSize)
-		buf := &bytes.Buffer{}
-
-		// Try open shared memory
+		
+		// Open shared memory (shm reader)
 		r, err := shm.Open(shmNamePhysics, physicsSize)
 		if err != nil {
 			fmt.Println("[ERROR] - Trying to open shm ", err)
 		}
-		//defer r.Close()
+		defer r.Close()
 		
-		// Try read the opened shared memory
-		_, err = r.Read(rbuf)
-		if err != nil {
-			fmt.Println("[ERROR] - Trying to read shm ", err)
-		}
 
-		buf.Write(rbuf)
-		err = binary.Read(buf, binary.LittleEndian, physics)
+		//----------------------------------------------------------------
+		// TODO: Currently testing Solution 1 & 2 below
+
+		// ---- Solution 1
+		// Read the opened shared memory into a raw buffer
+		// rbuf := make([]byte, physicsSize)
+		// _, err = r.Read(rbuf)
+		// if err != nil {
+		// 	fmt.Println("[ERROR] - Trying to read shm ", err)
+		// }
+
+		// Convert into a buffer wrapper for to decode into the struct
+		// buf := &bytes.Buffer{}
+		// buf.Write(rbuf)
+		// err = binary.Read(buf, binary.LittleEndian, physics)
+		// if err != nil {
+		// 	fmt.Println("[ERROR] - Trying to write shm ", err)
+		// }
+
+		// ---- Solution 2
+		err = binary.Read(r, binary.LittleEndian, physics)
 		if err != nil {
 			fmt.Println("[ERROR] - Trying to write shm ", err)
 		}
+
+		// ----------------------------------------------------------------
+		
 
 		err = r.Close()
 		if err != nil {
@@ -66,21 +81,33 @@ func ReadSharedMemory(physics *accshmdata.ACCPhysics) {
 	}
 }
 
-func SendDataViaUdp() {
-	conn, err := net.Dial("udp", SERVER_UDP_ADDR)
-	if err != nil {
-		fmt.Println("[ERROR] - Connecting to UDP Server: ", err)
-	}
-	defer conn.Close()
-
-	b := make([]byte, 1024)
+func SendDataViaUdp(physics *accshmdata.ACCPhysics) {
+	ticker := time.NewTicker(2 * time.Second)
+	defer ticker.Stop()
 
 	for {
-		_, err = conn.Write(b)
-		if err != nil {
-			fmt.Println("[ERROR] - Writing to UDP Server: ", err)
-		}
+		<-ticker.C
+
+		fmt.Println("RPM: ", physics.RPM)
+		fmt.Println("Gear: ", physics.Gear)
+		fmt.Println("Brake: ", physics.Brake)
+		fmt.Println("Speed: ", physics.SpeedKmh)
+		fmt.Println("--------------------")	
 	}
+	// conn, err := net.Dial("udp", SERVER_UDP_ADDR)
+	// if err != nil {
+	// 	fmt.Println("[ERROR] - Connecting to UDP Server: ", err)
+	// }
+	// defer conn.Close()
+
+	// b := make([]byte, 1024)
+
+	// for {
+	// 	_, err = conn.Write(b)
+	// 	if err != nil {
+	// 		fmt.Println("[ERROR] - Writing to UDP Server: ", err)
+	// 	}
+	// }
 }
 
 func main() {
@@ -90,31 +117,19 @@ func main() {
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	// for {
-	// 	<-ticker.C
 
-	// 	// Reading will fail, if the game has not been started at least once
-	// 	err := accshm.ReadPhysics(physics)
-	// 	if err != nil {
-	// 		fmt.Println("[ERROR] - Error reading frm client")
-	// 	}
+	// Reading will fail, if the game has not been started at least once
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ReadSharedMemory(physics)
+	}()
 
 	wg.Add(1)
 	go func() {
-		<-ticker.C
 		defer wg.Done()
-		ReadSharedMemory(physics)
-		fmt.Println("RPM: ", physics.RPM)
-		fmt.Println("Gear: ", physics.Gear)
-		fmt.Println("Brake: ", physics.Brake)
-		fmt.Println("--------------------")
+		SendDataViaUdp(physics)
 	}()
-
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	SendDataViaUdp()
-	// }()
 
 	wg.Wait()
 }
