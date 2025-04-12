6493fd4c8f18e1c63b196cb0fbb7527520af2a70
Afx31 <post4wjm@gmail.com>
2025-04-12 22:01:51 +1000

    House cleaning

--- udp-bridge/main.go
+++ udp-bridge/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	// "bytes"
 	"encoding/binary"
 	"fmt"
 	"math"
 	"net"
-
-	// "os"
 	"sync"
 	"unsafe"
 
@@ -30,11 +27,7 @@ var (
 func ReadSharedMemory(physics *accshmdata.ACCPhysics) {
 	fmt.Println("--- Reading Memory ---")
 	
-	// ticker := time.NewTicker(1 * time.Second)
-	// defer ticker.Stop()
-
 	for {
-		// <-ticker.C
 		physicsSize := (int32)(unsafe.Sizeof(*physics))
 		
 		// Open shared memory (shm reader)
@@ -87,32 +80,21 @@ func SendDataViaUdp(physics *accshmdata.ACCPhysics) {
 	}
 	defer conn.Close()
 
-	// ticker := time.NewTicker(1 * time.Second)
-	// defer ticker.Stop()
 	counter := 0
+
 	for {
-		// <-ticker.C
 		b := make([]byte, 30)
 
-		// fmt.Println("-------------------------------------------------")
-		// fmt.Println(counter)
 		binary.LittleEndian.PutUint32(b[0:4], uint32(physics.PacketId))
-		// fmt.Println("PacketId: ", physics.PacketId)
 		binary.LittleEndian.PutUint32(b[4:8], uint32(physics.RPM))
-		// fmt.Println("RPM: ", physics.RPM)
 		binary.LittleEndian.PutUint32(b[8:12], math.Float32bits(physics.SpeedKmh))
-		// fmt.Println("SpeedKmh: ", physics.SpeedKmh)
 		binary.LittleEndian.PutUint32(b[12:16], uint32(physics.Gear))
-		// fmt.Println("Gear: ", physics.Gear)
 		binary.LittleEndian.PutUint32(b[16:20], math.Float32bits(physics.Gas * 100))
-		// fmt.Println("Gas: ", physics.Gas)
-		
 
 		_, err = conn.Write(b)
 		if err != nil {
 			fmt.Println("[ERROR] - Writing to UDP Server: ", err)
 		}
-
 
 		counter++
 	}
@@ -122,11 +104,7 @@ func main() {
 	fmt.Println("----- Running -----")
 
 	physics := new(accshmdata.ACCPhysics)
-	// ticker := time.NewTicker(1 * time.Second)
-	// defer ticker.Stop()
 
-
-	// Reading will fail, if the game has not been started at least once
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
