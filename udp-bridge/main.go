3cd207ad59fb99fe2628b84ba1502ff041452456
Afx31 <post4wjm@gmail.com>
2025-04-15 07:21:28 +1000

    feat/Brake input

--- udp-bridge/main.go
+++ udp-bridge/main.go
@@ -18,6 +18,8 @@ const (
 	shmNameGraphics		= "Local\\acpmf_graphics"
 	shmNameStatic			= "Local\\acpmf_static"
 	SERVER_UDP_ADDR 	= ""
+	
+	PACKET_LENGTH			= 100
 )
 
 var (
@@ -83,19 +85,20 @@ func SendDataViaUdp(physics *accshmdata.ACCPhysics) {
 	counter := 0
 
 	for {
-		b := make([]byte, 30)
+		b := make([]byte, PACKET_LENGTH)
 
 		binary.LittleEndian.PutUint32(b[0:4], uint32(physics.PacketId))
 		binary.LittleEndian.PutUint32(b[4:8], uint32(physics.RPM))
 		binary.LittleEndian.PutUint32(b[8:12], math.Float32bits(physics.SpeedKmh))
 		binary.LittleEndian.PutUint32(b[12:16], uint32(physics.Gear))
 		binary.LittleEndian.PutUint32(b[16:20], math.Float32bits(physics.Gas * 100))
+		binary.LittleEndian.PutUint32(b[20:24], math.Float32bits(physics.Brake * 100))
 
 		_, err = conn.Write(b)
 		if err != nil {
 			fmt.Println("[ERROR] - Writing to UDP Server: ", err)
 		}
-
+    
 		counter++
 	}
 }
